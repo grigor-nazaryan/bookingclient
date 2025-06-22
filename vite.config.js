18c8841cbd383bf8b90f72121306a63815d23fd1
Grigor Nazaryan <nazaryang96@gmail.com>
2025-06-22 19:52:07 +0400

    vite confing changes

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
+
 export default defineConfig({
 	server: {
 		proxy: {
 			"/api": {
-				target: import.meta.env.VITE_API_URL || "http://localhost:4000",
-				secure: false // change to true when Prod
+				target: process.env.VITE_API_URL || "http://localhost:4000",
+				secure: process.env.NODE_ENV === 'production'
 			}
 		}
 	},
